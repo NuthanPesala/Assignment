7d8533a088b8e4c2c13dccc1bb35315cc2b73a1c
NuthanPesala <67287193+NuthanPesala@users.noreply.github.com>
2020-10-17 19:48:41 +0530

    Initial Commit

---
+++ Assignment/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Assignment
+//
+//  Created by Nuthan Raju Pesala on 17/10/20.
+//  Copyright © 2020 NuthanRaju. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
